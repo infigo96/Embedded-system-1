87205da98c5ae96b3a3052e75d661b07fe747234
Albert B <albert.k.bergstrom@hotmail.com>
2018-09-17 18:24:52 +0200

    Added TimeConverter

--- Lab3/Lab3/src/Utilities.c
+++ Lab3/Lab3/src/Utilities.c
@@ -5,8 +5,18 @@
  *  Author: Albert
  */ 
 #include "Utilities.h"
+//Divides total number of seconds into seconds, minuts and hours. Then, outputs it as a string
 void Convert_Sec_To_String(char *timestring){
-	volatile int secounds, minuts, hours;
+	char seconds[4], minuts[5], hours[5];
+	char output[12];
+
+
+	sprintf(hours, "%dh ", (time / 3600) % 24);
+	sprintf(minuts, "%dm ", (time / 60) % 60);
+	sprintf(seconds, "%ds", time % 60);
 	
-	secounds = time % 60;
+	strcat(output,hours);
+	strcat(output,minuts);
+	strcat(output,seconds);
+	strcpy(timestring, output);
 }
--- Lab3/Lab3/src/Utilities.h
+++ Lab3/Lab3/src/Utilities.h
@@ -4,10 +4,11 @@
  * Created: 2018-09-17 17:18:15
  *  Author: Albert
  */ 
-
-volatile int time;
+int time;
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
+#include <stdio.h>
+#include <string.h>
 void Convert_Sec_To_String(char *timestring);
 
 
