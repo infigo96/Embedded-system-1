68ea7f8766927097794c9af451926a8fa7e7b826
Albert B <albert.k.bergstrom@hotmail.com>
2018-09-24 11:15:47 +0200

    Lab 4.1 somewhat finished

--- Lab4/Lab4/Lab4/src/main.c
+++ Lab4/Lab4/Lab4/src/main.c
@@ -4,7 +4,7 @@
  Copyright     : GPL
  Description   : EVK1100 template
  **********************************************************/
-
+#define SAMPELS 10
 // Include Files
 #include "board.h"
 #include "adc.h"
@@ -21,33 +21,67 @@ void mdelay(int ms)		//Delay function
 }
 int main(void)
 {
-	int pot_value;
+	volatile int pot_value; //Potentiometer
+	volatile int lig_value; //Light
+	volatile int tem_value; //Temperature
 	int brightness_LED5;
 
 	// Configure the ADC module and enable the potentiometer channel
 	adc_configure(&AVR32_ADC);
 	adc_enable(&AVR32_ADC, ADC_POTENTIOMETER_CHANNEL);
-   
-	// Start a ADC sampling of all active channels
-	adc_start(&AVR32_ADC);
-
-	// Get the potentiometer value
-	pot_value = adc_get_value(&AVR32_ADC, ADC_POTENTIOMETER_CHANNEL);
-
-	// Convert the potentiometer value to a value btwn 0-255
-	brightness_LED5 = pot_value * 255 / 1024;
-
-	// Set the intensity of the LED
-	LED_Set_Intensity(LED5, brightness_LED5);
+    adc_enable(&AVR32_ADC, ADC_LIGHT_CHANNEL);
+	adc_enable(&AVR32_ADC, ADC_TEMPERATURE_CHANNEL);
 	
+
+	
+
+
+	//// Convert the potentiometer value to a value btwn 0-255
+	//brightness_LED5 = pot_value * 255 / 1024;
+//
+	//// Set the intensity of the LED
+	//LED_Set_Intensity(LED5, brightness_LED5);
+	delay_init();
 	display_init();
 	char text[25] = "Hello world";
-	while(true)
+	
+	while(1)
 	{
+		lig_value = 0;
+		tem_value = 0;
+		for (int i=0;i<SAMPELS;i++)
+		{
+			adc_start(&AVR32_ADC); // Start a ADC sampling of all active channels
+			lig_value += adc_get_value(&AVR32_ADC, ADC_LIGHT_CHANNEL); // Get the light sensor value
+			tem_value += adc_get_value(&AVR32_ADC, ADC_TEMPERATURE_CHANNEL); // Get the temperature sensor value
+		}
+		// Get the average
+		lig_value /= SAMPELS;
+		tem_value /= SAMPELS;
+		// Get the potentiometer value
+		pot_value = adc_get_value(&AVR32_ADC, ADC_POTENTIOMETER_CHANNEL);
+
+
+		
+		//Clear the display
+		//dip204_clear_display();
+		
+		//Write first row
 		dip204_set_cursor_position(1,1);
-		sprintf(text,"%d",pot_value);
+		sprintf(text,"%04d",pot_value);
 		dip204_printf_string(text);
-		delay_s(1);
+		
+		//Write second row
+		dip204_set_cursor_position(1,2);
+		sprintf(text,"%04d",lig_value);
+		dip204_printf_string(text);
+
+
+		//Write third row
+		dip204_set_cursor_position(1,3);
+		sprintf(text,"%04d",tem_value);
+		dip204_printf_string(text);
+		delay_ms(1000);
 	}
 
 	// Never return from main
