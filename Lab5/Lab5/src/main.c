e570c6c84b765a353ab125c81e4aa888f1eec585
Albert B <albert.k.bergstrom@hotmail.com>
2018-09-27 11:11:45 +0200

    Lab5 init

---
+++ Lab5/Lab5/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ * Created: 2018-09-27 09:50:57
+ *  Author: Albert
+ */ 
+#include "FreeRTOS.h"
+#include "task.h"
+
+int main(){
+	
+	while(1)
+	{
+		
+	}
+	
+	
+	
+	
+	return 0;
+}
