4d2ac8b9a095ad1a296dd34332680383ed9b1187
Hampus <hbz15001@student.mdh.se>
2018-09-27 13:47:58 +0200

    First light of led

--- Lab5/Lab5/src/led_tasks.c
+++ Lab5/Lab5/src/led_tasks.c
@@ -10,17 +10,17 @@ void initLED()
 	volatile avr32_gpio_port_t *led_port;
 	led_port = & AVR32_GPIO.port[LED_PORT];
 	
-	led_port->gpers = LED0_PIN;
-	led_port->gpers = LED1_PIN;
-	led_port->gpers = LED2_PIN;
+	led_port->gpers = 1 << LED0_PIN;
+	led_port->gpers = 1 << LED1_PIN;
+	led_port->gpers = 1 << LED2_PIN;
 
-	led_port->ovrs = LED0_PIN;
-	led_port->ovrs = LED1_PIN;
-	led_port->ovrs = LED2_PIN;
+	led_port->ovrs = 1 << LED0_PIN;
+	led_port->ovrs = 1 << LED1_PIN;
+	led_port->ovrs = 1 << LED2_PIN;
 
-	led_port->oders = LED0_PIN;
-	led_port->oders = LED1_PIN;
-	led_port->oders = LED2_PIN;
+	led_port->oders = 1 << LED0_PIN;
+	led_port->oders = 1 << LED1_PIN;
+	led_port->oders = 1 << LED2_PIN;
 }
 void vBlinkLED1( void * pvParameters )
 {
@@ -28,7 +28,7 @@ void vBlinkLED1( void * pvParameters )
 	while (1)
 	{
 		AVR32_GPIO.port[LED_PORT].ovrt = (1 << LED0_PIN);
-		vTaskDelay( 100 );
+		vTaskDelay( 1000 );
 	}
 		
 }
--- Lab5/Lab5/src/main.c
+++ Lab5/Lab5/src/main.c
@@ -1,18 +1,15 @@
-/*
- * main.c
- *
- * Created: 2018-09-27 09:50:57
- *  Author: Albert
- */ 
 
 #include "led_tasks.h"
 
 int main()
 {
+	initLED();
+	AVR32_GPIO.port[LED_PORT].ovrt = (1 << LED0_PIN);
 	static unsigned char ucParameterToPass;
 	int i = 0;
 	xTaskHandle xHandle;
 	xTaskCreate(vBlinkLED1,"Blink1",configMINIMAL_STACK_SIZE,&ucParameterToPass,tskIDLE_PRIORITY + 1,&xHandle);
+	
 	vTaskStartScheduler();
 	while(1)
 	{
